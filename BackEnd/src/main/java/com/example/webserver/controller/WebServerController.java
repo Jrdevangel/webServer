c2d8e991be3139713bea225be7dcfd13fd126000
Ángel Ponce <angelillopr98@icloud.com>
2025-10-24 12:54:11 +0200

    Clean imports in WebServerController by unused Redirect import

--- BackEnd/src/main/java/com/example/webserver/controller/WebServerController.java
+++ BackEnd/src/main/java/com/example/webserver/controller/WebServerController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.example.webserver.service.DashboardService;
 
@@ -19,6 +20,11 @@ public class WebServerController {
     
     public WebServerController(DashboardService dashboardService) {
         this.dashboardService = dashboardService;
+    }
+
+    @GetMapping("/")
+    public RedirectView redirectToHome() {
+        return new RedirectView("/home");
     }
 
     @GetMapping("/home")
