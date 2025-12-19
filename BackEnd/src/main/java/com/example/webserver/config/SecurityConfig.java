2af648383b338433649e2fbf777aa6f459e1e5c7
Ángel Ponce <angelillopr98@icloud.com>
2025-12-19 12:27:19 +0100

    Refactor SecurityConfig to use PasswordEncoder and external UserDetailsService

--- BackEnd/src/main/java/com/example/webserver/config/SecurityConfig.java
+++ BackEnd/src/main/java/com/example/webserver/config/SecurityConfig.java
@@ -5,10 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -61,21 +59,11 @@ public class SecurityConfig {
             );
 
         return http.build();
-    }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-
-        UserDetails admin = User.withUsername(adminUsername)
-                .password("{noop}" + adminPassword)
-                .roles("ADMIN")
-                .build();
-
-        UserDetails normalUser = User.withUsername(userUsername)
-                .password("{noop}" + userPassword)
-                .roles("USER")
-                .build();
-
-        return new InMemoryUserDetailsManager(admin, normalUser);
-    }
+    } 
+        
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
 }
