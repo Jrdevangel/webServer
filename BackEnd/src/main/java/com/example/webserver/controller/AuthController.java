a1561e3bab5b8ef9f91bbedf395b88f1ac5e0942
Ángel Ponce <angelillopr98@icloud.com>
2026-03-09 21:23:26 +0100

    Implement user registration endpoint and add RegisterRequest DTO

--- BackEnd/src/main/java/com/example/webserver/controller/AuthController.java
+++ BackEnd/src/main/java/com/example/webserver/controller/AuthController.java
@@ -1,7 +1,11 @@
 package com.example.webserver.controller;
 
+import com.example.webserver.domain.Role;
+import com.example.webserver.domain.UserEntity;
 import com.example.webserver.dto.LoginRequest;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.webserver.dto.RegisterRequest;
+import com.example.webserver.service.UserService;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
@@ -11,8 +15,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
+    private final AuthenticationManager authenticationManager;
+    private final UserService userService;
+
+    public AuthController(AuthenticationManager authenticationManager,
+                          UserService userService) {
+        this.authenticationManager = authenticationManager;
+        this.userService = userService;
+    }
 
     @PostMapping("/login")
     public String login(@RequestBody LoginRequest request) {
@@ -20,15 +30,29 @@ public class AuthController {
         try {
             authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
-                    request.getUsername(),
-                    request.getPassword()
+                        request.getUsername(),
+                        request.getPassword()
                 )
-           );  
+            );
 
             return "Correct login";
 
         } catch (AuthenticationException e) {
             return "Incorrect credentials";
         }
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestBody RegisterRequest request) {
+
+        UserEntity user = new UserEntity(
+                request.getUsername(),
+                request.getPassword(),
+                Role.USER
+        );
+
+        userService.saveUser(user);
+
+        return "User registered";
     }
 }
