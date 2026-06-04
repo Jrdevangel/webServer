e66e55734e5d2b5f552c52ea7a6a325942135046
Ángel Ponce <angelillopr98@icloud.com>
2026-06-04 13:06:28 +0200

    Clean Swagger documentation and internal endpoints

--- BackEnd/src/main/java/com/example/webserver/controller/WebServerController.java
+++ BackEnd/src/main/java/com/example/webserver/controller/WebServerController.java
@@ -2,6 +2,7 @@ package com.example.webserver.controller;
 
 import java.security.Principal;
 import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +10,14 @@ import org.springframework.web.servlet.view.RedirectView;
 
 import com.example.webserver.service.DashboardService;
 
+import io.swagger.v3.oas.annotations.Hidden;
+
 @RestController
+@Hidden
 public class WebServerController {
 
     private final DashboardService dashboardService;
-    
+
     public WebServerController(DashboardService dashboardService) {
         this.dashboardService = dashboardService;
     }
@@ -29,10 +33,11 @@ public class WebServerController {
         return "Welcome, " + name + "!";
     }
 
-@PostMapping("/dashboard")
-public Map<String, String> postDashboardMessage(Principal user) {
-    String name = (user != null) ? user.getName() : "Guest";
-    String message = dashboardService.dashboardMessage(name);
-    return Map.of("message", message);
-}
+    @PostMapping("/dashboard")
+    public Map<String, String> postDashboardMessage(Principal user) {
+        String name = (user != null) ? user.getName() : "Guest";
+        String message = dashboardService.dashboardMessage(name);
+
+        return Map.of("message", message);
+    }
 }
